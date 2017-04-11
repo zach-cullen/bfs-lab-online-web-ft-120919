916ae670c12349045084ce4b79974e52e585d54e
Jeff Katz <jeffreyerickatz@gmail.com>
2017-04-10 22:17:24 -0400

    update the visited nodes png

--- test/index-test.js
+++ test/index-test.js
@@ -1,103 +1,54 @@
 var chai = require('chai');
 var sinon = require('sinon');
 
-beforeEach(function() {
-  expect.spyOn(console, 'log')
-})
+let edges = [
+	['14th&6th', '23rd&6th'],
+	['23rd&6th', '34th&6th'],
+	['34th&6th', '28th&Bwy'],
+	['28th&Bwy', '23rd&Bwy'],
+	['23rd&Bwy', '14th&Lex'],
+	['14th&Lex', '23rd&Lex'],
+	['23rd&Lex', '28th&Lex'],
+	['28th&Lex', '33rd&Lex']
+]
 
-afterEach(function() {
-  expect.restoreSpies()
-})
+let vertices = [
+  {name: '34th&6th', distance: null, predecessor: null},
+  {name: '23rd&6th', distance: null, predecessor: null},
+  {name: '14th&6th', distance: null, predecessor: null},
+  {name: '34th&6th', distance: null, predecessor: null},
+  {name: '28th&Bwy', distance: null, predecessor: null},
+  {name: '14th&Lex', distance: null, predecessor: null}
+]
 
-describe('#printString', function() {
-  it("should print all of the ", function() {
-    printString('pizza')
-
-    expect(console.log).toHaveBeenCalledWith("p")
-    expect(console.log).toHaveBeenCalledWith("i")
-    expect(console.log).toHaveBeenCalledWith("z")
-    expect(console.log).toHaveBeenCalledWith("z")
-    expect(console.log).toHaveBeenCalledWith("a")
+describe('#findAdjacentNodes', function() {
+  it("should return an array of adjacent nodes", function() {
+    expect(findAdjacent('34th&6th',  vertices, edges)).toEqual([{name: '23rd&6th', distance: null, predecessor: null},
+      {name: '28th&Bwy', distance: null, predecessor: null}
+    ])
   });
 
-  it("calls the function once for each letter in the string", function() {
-    var printString = sinon.spy(window, "printString");
-    printString("pizza")
-    expect(printString.callCount).toEqual(5)
-  })
-
-  it("adds item dynamically", function() {
-    addToCart('pizza');
-    expect(getCart()[0]['item']).toEqual(undefined)
-  })
-});
-
-describe('#reverseString', function() {
-  it("should reverse all of the letters of a string", function() {
-    expect(reverseString('pizza')).toEqual("azzip")
-  });
-
-  it("should make the proper recursive calls", function() {
-    var reverseString = sinon.spy(window, "reverseString");
-    reverseString("pizza")
-    expect(reverseString.callCount).toEqual(5)
+  it("excludes discovered nodes", function() {
+    expect(findAdjacent('28th&Bwy',  vertices, edges)).toEqual()
+    // but these nodes are not in the node list?
+    // 23rd&Broadway
+    // 33rd&Lex
   })
 });
 
+describe('#markDistanceAndPredecessor', function() {
+  it("should return an array of adjacent nodes", function() {
+    let twentyThirdAndSixth = vertices[1]
+    let twentyEighthAndBroadway = vertices[4]
+    let thirtyFourthAndSixth = vertices[0]
+    let adjacentNodes = [twentyThirdAndSixth, twentyEighthAndBroadway]
 
-describe('#isPalindrome', function() {
-  it("should return false when a string is not a palindrome", function() {
-    expect(isPalindrome('pizza')).toEqual(false)
+    markDistanceAndPredecessor(thirtyFourthAndSixth, adjacentNodes)
+
+    expect(twentyThirdAndSixth.distance).toEqual(1)
+    expect(twentyEighthAndBroadway.distance).toEqual(1)
+    expect(twentyEighthAndBroadway.predecessor).toEqual(thirtyFourthAndSixth)
   });
 
-  it("should return true when a string is a palindrome", function() {
-    expect(isPalindrome("madamimadam")).toEqual(true)
-  });
 
-  it("should make the proper recursive calls", function() {
-    var isPalindrome = sinon.spy(window, "isPalindrome");
-    isPalindrome("madamimadam")
-    expect(isPalindrome.callCount).toEqual(6)
-  })
-});
-
-describe('#addUpTo', function() {
-  it("should add up to a given index in an array", function() {
-    expect(addUpTo([1, 4, 5, 3], 2)).toEqual(10)
-  });
-
-  it("should make the proper recursive calls", function() {
-    var addUpTo = sinon.spy(window, "addUpTo");
-    addUpTo([1, 4, 5, 3], 2)
-    expect(addUpTo.callCount).toEqual(3)
-  });
-});
-
-describe('#maxOf', function() {
-  it("should find the maximum integer in an array", function() {
-    expect(maxOf([1, 4, 5, 3])).toEqual(5)
-  });
-
-  it("should make the proper recursive calls", function() {
-    var maxOf = sinon.spy(window, "maxOf");
-    maxOf([1, 4, 5, 3])
-    expect(maxOf.callCount).toEqual(4)
-  });
-});
-
-
-describe('#includesNumber', function() {
-  it("should return true if the number is included in the array", function() {
-    expect(includesNumber([1, 4, 5, 3], 5)).toEqual(true)
-  });
-
-  it("should return false if the number is not included in the array", function() {
-    expect(includesNumber([1, 4, 2, 3], 5)).toEqual(false)
-  });
-
-  it("should make the proper recursive calls", function() {
-    var includesNumber = sinon.spy(window, "includesNumber");
-    includesNumber([1, 4, 5, 3], 3)
-    expect(includesNumber.callCount).toEqual(4)
-  });
 });
